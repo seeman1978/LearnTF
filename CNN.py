9288e1618f0c5a9c3479c1a8b302c49fd3647e39
seeman1978 <seeman1978@163.com>
2020-04-25 20:06:28 +0800

    My VGG13

--- CNN.py
+++ CNN.py
@@ -109,6 +109,19 @@ if __name__ == "__main__":
     train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
     train_data = train_data.repeat().shuffle(5000).batch(batch_size).prefetch(1)
 
+    # 获取所有GPU 设备列表
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # 设置GPU 显存占用为按需分配
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # 异常处理
+            print(e)
+
     # Build neural network model.
     conv_net = ConvNet()
 
