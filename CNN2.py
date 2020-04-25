95de7a90d8b82c5b4c3c5d34067a1c2b04d4910c
seeman1978 <seeman1978@163.com>
2020-04-25 20:04:09 +0800

    My VGG13

--- CNN2.py
+++ CNN2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import backend as K
 from tensorflow.keras.models import Model, Sequential
@@ -19,6 +20,19 @@ if __name__ == "__main__" :
     # if we use sparse_categorical_crossentropy then we can skip this step.
     y_train = keras.utils.to_categorical(y_train, num_classes)
     y_test = keras.utils.to_categorical(y_test, num_classes)
+
+    # 获取所有GPU 设备列表
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # 设置GPU 显存占用为按需分配
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # 异常处理
+            print(e)
 
     # 构建模型
     inp = Input(shape=input_shape)
